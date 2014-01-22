3bcacd9a5dd47d81f5459159599557bfe8a30724
root <root@dinoop.(none)>
2014-01-22 12:38:43 +0530

    added runserver file

---
+++ runserver.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Flask Web Object
+# runserver.py
+
+import os
+from API import webapp
+
+if __name__ = "__main__":
+   webapp.run('0.0.0.0', port=8000)
